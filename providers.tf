2c65acd48053dc18d0beaace3b221dc86362ae8a
kasim <kasimshaikhh98@gmail.com>
2025-07-26 13:14:00 +0530

    Update providers.tf
    ami update

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ provider "aws" {
 
 }
 resource "aws_instance" "jenkins" {
-  ami           = "ami-09191d47657c9691a" 
+  ami           = "ami-0a72753edf3e631b7" 
   instance_type = "t2.micro"
   tags = {
     Name = "JenkinsInstance"
